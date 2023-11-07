6e9ef7c36a50d3921593eb4cda6c45a2039a413d
Jiajun Li <1009839598@qq.com>
2023-11-07 03:05:13 -0600

    routing

--- App.js
+++ App.js
@@ -1,71 +1,19 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Input, CheckBox, Icon, Header} from '@rneui/themed'
-import {Button} from "@rneui/base";
+import * as React from 'react';
+import Login from './Login'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Personal from "./Personal";
 
-export default class Login extends Component {
-
-    render() {
-        return (
-        <>
-            <Header
-                backgroundColor="green"
-                backgroundImageStyle={{}}
-                barStyle="default"
-                centerComponent={{
-                    text: "RateScope",
-                    style: { color: "#fff", fontSize: 40 },
-                }}
-                centerContainerStyle={{}}
-                containerStyle={{ }}
-                // leftComponent={{ icon: "menu", color: "#fff" }}
-                leftContainerStyle={{}}
-                linearGradientProps={{}}
-                placement="center"
-                // rightComponent={{ icon: "home", color: "#fff" }}
-                rightContainerStyle={{}}
-                statusBarProps={{}}
-
-            />
-
-            <View style={styles.container}>
+const Stack = createNativeStackNavigator();
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Personal" component={Personal} />
+            </Stack.Navigator>
 
 
-                <Input placeholder='Email' />
-
-                <Input placeholder='Password' secureTextEntry={true}/>
-
-                <CheckBox title='Remember password'/>
-
-                <Button title="Login"
-                        buttonStyle={{
-                            backgroundColor: "rgba(92, 99,216, 1)",
-                            borderRadius: 5,
-                        }}
-                        containerStyle={{
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            marginVertical: 10
-                        }}
-                />
-
-                <Text style={{textAlign: 'center', textDecorationLine: 'underline'}}
-                      onPress={'#'}>
-                    Forgot your password?
-                </Text>
-
-
-            </View>
-        </>
-
-        );
-    }
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginVertical: 100,
-        paddingHorizontal: 10,
-    }
-})
---
+++ Login.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Input, CheckBox, Icon, Header} from '@rneui/themed'
+import {Button} from "@rneui/base";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default class Login extends Component {
+
+    render() {
+        return (
+            <>
+                <Header
+                    backgroundColor="green"
+                    backgroundImageStyle={{}}
+                    barStyle="default"
+                    centerComponent={{
+                        text: "RateScope",
+                        style: { color: "#fff", fontSize: 40 },
+                    }}
+                    centerContainerStyle={{}}
+                    containerStyle={{ }}
+                    // leftComponent={{ icon: "menu", color: "#fff" }}
+                    leftContainerStyle={{}}
+                    linearGradientProps={{}}
+                    placement="center"
+                    // rightComponent={{ icon: "home", color: "#fff" }}
+                    rightContainerStyle={{}}
+                    statusBarProps={{}}
+
+                />
+
+                <View style={styles.container}>
+
+
+                    <Input placeholder='Email' />
+
+                    <Input placeholder='Password' secureTextEntry={true}/>
+
+                    <CheckBox title='Remember password'/>
+
+                    <Button title="Login"
+                            buttonStyle={{
+                                backgroundColor: "rgba(92, 99,216, 1)",
+                                borderRadius: 5,
+                            }}
+                            containerStyle={{
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                marginVertical: 10
+                            }}
+                            onPress={() => this.props.navigation.navigate('Personal')}
+                    />
+
+                    <Text style={{textAlign: 'center', textDecorationLine: 'underline'}}
+                          onPress={'#'}>
+                        Forgot your password?
+                    </Text>
+
+
+                </View>
+            </>
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginVertical: 100,
+        paddingHorizontal: 10,
+    }
+})
